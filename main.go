93a2d3b8c32e51acefd4b9d5fc63e768744cd1f8
astrviktor <astrviktor@gmail.com>
2023-03-19 13:00:01 +0300

    hw01 step 3 done

---
+++ main.go
@@ -0,0 +1,5 @@
+package main
+
+func SqueareRoot(a, b, c float64) []float64 {
+	return nil
+}
---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSqueareRoot(t *testing.T) {
+	tests := []struct {
+		a, b, c  float64
+		expected []float64
+	}{
+		{1, 0, 1, []float64{}},
+	}
+
+	for _, test := range tests {
+		result := SqueareRoot(test.a, test.b, test.c)
+		require.Equal(t, test.expected, result)
+	}
+}
