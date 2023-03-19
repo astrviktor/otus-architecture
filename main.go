d45ca8a2641cc11522323f9201f2812b3613a114
astrviktor <astrviktor@gmail.com>
2023-03-19 13:23:14 +0300

    hw01 step 8 done

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "math"
 
 func SqueareRoot(a, b, c float64) []float64 {
+	const epsilon = 0.0000001
 	D := b*b - 4*a*c
+
+	if math.Abs(D) < epsilon {
+		return []float64{
+			-b / (2 * a),
+		}
+	}
 
 	if D < 0 {
 		return []float64{}
